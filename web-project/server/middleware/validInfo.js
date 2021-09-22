cd9b427ba303da7bbb346f7df3b9602061bc1a14
GorbachevaPolina <gorbacheva.polina7394@gmail.com>
2021-09-22 16:24:15 +0300

    started client side and added registration page

--- web-project/server/middleware/validInfo.js
+++ web-project/server/middleware/validInfo.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
     const { email, username, password } = req.body;
-  
     function validEmail(userEmail) {
       return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
     }
